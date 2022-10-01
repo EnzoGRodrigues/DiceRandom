951bde37342dbeebc6d1281392a3a0d219f4c189
Enzo Rodrigues <enzorodrigues272@icloud.com>
2022-10-01 18:21:49 -0300

    implementando auto layout e sistema random

--- DiceRandom/ViewController.swift
+++ DiceRandom/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    
+    @IBAction func buttomClickMe(_ sender: Any) {
+        
+        let randomFirstDice = [UIImage(imageLiteralResourceName: "dice1"), UIImage(imageLiteralResourceName: "dice2"), UIImage(imageLiteralResourceName: "dice3"), UIImage(imageLiteralResourceName: "dice4"), UIImage(imageLiteralResourceName: "dice5"), UIImage(imageLiteralResourceName: "dice6")]
+        
+        diceOne.image = randomFirstDice.randomElement()
+        diceTwo.image = randomFirstDice.randomElement()
+    }
+    
+    
 }
 
